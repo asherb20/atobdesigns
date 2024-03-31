8b5968e8e61907450834c87e010a3e1f82ca1cb5
Asher Best <ashermcbest@gmail.com>
2024-03-31 18:01:27 -0500

    Finished portfolio page.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,12 +4,12 @@ import CallToAction from './cta';
 const Hero = ({ title }: { title: string }) => {
   return (
     <div>
-      <div className='pt-5 pr-2 pb-5 pl-2 sm-d-flex sm-flex-wrap max-w-lg m-auto'>
-        <div className='sm-flex-1'>
+      <div className='pt-10 pr-2 pb-10 pl-2 sm-d-flex sm-flex-wrap max-w-lg m-auto'>
+        <div className='sm-flex-2'>
           <p className='fs-40 fw-bold pb-1'>{title}</p>
           <CallToAction to='/contact-us' title='Book Free Consultation' />
         </div>
-        <div className='p-2 sm-flex-1' style={{ height: 116 }}>
+        <div className='p-2 sm-flex-1 d-none sm-d-block' style={{ height: 116 }}>
           <div>
             <div className='pos-rel my-auto'>
               <div className='square bg-dark pos-abs top-0 left-0 z-1' />
